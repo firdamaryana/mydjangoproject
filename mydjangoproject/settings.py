091e1eafbe5e8854f1aae6bf1eb67ac17a211e2d
firdamaryana <firda_maryana94@yahoo.com>
2017-03-31 14:48:15 +0700

    edit library

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -117,9 +117,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/blog/'
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
